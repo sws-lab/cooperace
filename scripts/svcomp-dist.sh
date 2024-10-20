316db2f9ebd3a6e698361086e478d1cf0990cb9c
Vesal Vojdani <vesal.vojdani@gmail.com>
2024-10-20 18:36:59 +0000

    Add a couple of basic tests to dist

--- scripts/svcomp-dist.sh
+++ scripts/svcomp-dist.sh
@@ -5,12 +5,15 @@
 
 ROOT=`pwd`
 DIST="dist/cooperace"
-mkdir -p $DIST
+mkdir -p $DIST/tests
 
 cp cooperace $DIST
 git ls-files src | xargs -I {} cp --parents {} $DIST
 cp -r tools $DIST/tools
 cp LICENSE README.md $DIST
+
+cp tests/properties/no-data-race.prp $DIST/tests
+cp tests/no-data-race/00-sanity_09-include.i $DIST/tests/test.i
 
 cd $DIST/..
 zip -r cooperace.zip cooperace
